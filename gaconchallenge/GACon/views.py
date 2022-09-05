b4c5d6153cc5644ea2dcf75a25bb6b078dd3fa3d
Mohammed Soubaneh <soubcompany@gmail.com>
2022-09-05 14:28:54 -0600

    check

--- gaconchallenge/GACon/views.py
+++ gaconchallenge/GACon/views.py
@@ -24,4 +24,3 @@ class TalkList(APIView):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
